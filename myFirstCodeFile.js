491bb59e43cec84f8a67176d2c3863481b272ca0
Jonah Bergevin <jonahbergevin@MacBook-Pro.local>
2016-05-09 14:41:56 -0600

    Once more commit

--- myFirstCodeFile.js
+++ myFirstCodeFile.js
@@ -1,6 +1,9 @@
 var dm11 = "started today";
 var journal = [];
-journal.push(dm11);
 var myDay = "8hours";
 var javaScript = "Really Hard but I shall conquer";
+var bar = "car";
+
 journal.push(myDay);
+journal.push(dm11);
+journal.push(car);
